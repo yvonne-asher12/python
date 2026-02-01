0299ecc75a4436e20b610db22abcfd8cf876078b
yvonne-asher12 <yvonneasher12@gmail.com>
2026-02-01 19:05:38 +0300

    second commit

---
+++ Dictionary.py
@@ -0,0 +1,29 @@
+
+invt={}
+i = 1
+
+while True:
+ item = input(f"Enter item name {i}:")
+ if item == "":
+   break #it stops loop if user presses enter
+ quantity = int(input(f"Enter the quantity {i}:"))
+ i += 1
+
+ #allowing user to update quantity
+ invt[item] = invt.get(item,0) + quantity
+
+print (invt)
+
+#retrieving of items
+retrieve_item = input("Enter item to retrieve:")
+if retrieve_item in invt:
+     print(f"{retrieve_item} quantity: {invt[retrieve_item]}")
+else:
+         print(f"{retrieve_item} not found in the invt")
+         
+#calculating the total quantity
+tot_qty = sum(invt.values())
+print(tot_qty)
+
+
+
