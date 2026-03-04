8c5d792d3b95d610bd449afd3373a7805d4259ef
yvonne-asher12 <yvonneasher46@gmail.com>
2026-03-04 04:49:52 +0300

    Update Bankacc.py

--- Bankacc.py
+++ Bankacc.py
@@ -1,29 +1,37 @@
-Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
->>> 
-=== RESTART: C:/Users/Comp/AppData/Local/Programs/Python/Python314/bankacc.py ==
-30000
-10000
-32000
-246
-32000
-04-02-2026
-Asher
->>> 
-=== RESTART: C:/Users/Comp/AppData/Local/Programs/Python/Python314/bankacc.py ==
-30000
-10000
-account balance: 32000
-acc_no: 246
-balance: 32000
-date:: 04-02-2026
-customer_name: Asher
->>> 
-=== RESTART: C:/Users/Comp/AppData/Local/Programs/Python/Python314/bankacc.py ==
-30000
-10000
-account balance: 32000
-acc_no: 246
-balance: 32000
-date:: 04-02-2026
-customer_name: Asher
+#Yvonne Karimi
+#BSCIT-05-0072/2024
+class BankAccount:
+    
+    def __init__(self, acc_no, balance, dof_opening,cust_name):
+        self.acc_no = acc_no
+        self.balance = balance
+        self.dof_opening = dof_opening
+        self.cust_name = cust_name
+    
+    def deposit (self,amount):
+        self.balance += amount
+        return amount
+    def withdrawal (self,amount):
+        if amount > self.balance:
+            return "Insufficient balance"
+        else:
+            self.balance -= amount
+            return amount
+    def check_balance(self):
+        print("account balance:",self.balance)
+    def cust_details(self):
+        print("acc_no:",self.acc_no)
+        print("balance:",self.balance)
+        print("date::",self.dof_opening)
+        print("customer_name:",self.cust_name)
+
+
+acc = BankAccount("246" ,12000, "04-02-2026", "Asher")
+
+print(acc.deposit(30000))
+print(acc.withdrawal(10000))
+
+acc.check_balance()
+acc.cust_details()
+    
+
