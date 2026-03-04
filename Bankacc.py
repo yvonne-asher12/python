b992e64a78c8e02b46693f75be250c05abd52496
yvonne-asher12 <yvonneasher12@gmail.com>
2026-03-04 04:40:37 +0300

    acc commit

---
+++ Bankacc.py
@@ -0,0 +1,29 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+=== RESTART: C:/Users/Comp/AppData/Local/Programs/Python/Python314/bankacc.py ==
+30000
+10000
+32000
+246
+32000
+04-02-2026
+Asher
+>>> 
+=== RESTART: C:/Users/Comp/AppData/Local/Programs/Python/Python314/bankacc.py ==
+30000
+10000
+account balance: 32000
+acc_no: 246
+balance: 32000
+date:: 04-02-2026
+customer_name: Asher
+>>> 
+=== RESTART: C:/Users/Comp/AppData/Local/Programs/Python/Python314/bankacc.py ==
+30000
+10000
+account balance: 32000
+acc_no: 246
+balance: 32000
+date:: 04-02-2026
+customer_name: Asher
